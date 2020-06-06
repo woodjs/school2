00470c3715bc2952aca606cc5124e5b0395b4e5a
woody <45289944+woody21978@users.noreply.github.com>
2020-06-06 21:28:45 +0300

    new commit

--- js/login.js
+++ js/login.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $('.form-link__reg').click(function() {
-    // $('form').animate({height: "toggle", opacity: "toggle"}, "show");
-    $('.form-reset').slideToggle();
-    $('.form-auth').slideToggle();
-    $('.form-reg').slideToggle();
+    $('form').animate({height: "toggle", opacity: "toggle"}, "show");
+    // $('.form-reset').slideToggle();
+    // $('.form-auth').slideToggle();
+    // $('.form-reg').slideToggle();
     $('.form').addClass( "form--reg" );
  });
-  $('.form-link__reset').click(function() {
-    $('.form-reset').slideToggle();
-    $('.form-auth').slideToggle();
-  });
+  // $('.form-link__reset').click(function() {
+  //   $('.form-reset').slideToggle();
+  //   $('.form-auth').slideToggle();
+  // });
 });
--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ $(document).ready(function() {
     selector: '#Calendar',
     date: new Date(),
     todaysDate: new Date()
-})
+  });
+  $('.change-password').click(function() {
+    $('.modal').show();
+  });
+  $('.modal-close').click(function() {
+    $('.modal').hide();
+  });
 });
